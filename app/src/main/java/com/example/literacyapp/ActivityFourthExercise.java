a49b2d01bc2b8e0203a0c6c73ef54704f6333d8e
Khaked Mamdouh <khaledmamdouh082@gmail.com>
2019-11-20 12:00:08 +0200

    hi

--- app/src/main/java/com/example/literacyapp/ActivityFourthExercise.java
+++ app/src/main/java/com/example/literacyapp/ActivityFourthExercise.java
@@ -9,14 +9,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ActivityFourthExercise extends AppCompatActivity implements AdapterFourthExercise.OnItemClickListener {
     RecyclerView recyclerView;
     ArrayList<ModelFourthExercise> modelFourthExercise;
     AdapterFourthExercise adapterFourthExercise;
     ImageView wordImage;
+    TextView firstoption;
+    TextView secondoption;
+    TextView thirdoption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +29,28 @@ public class ActivityFourthExercise extends AppCompatActivity implements Adapter
         setContentView(R.layout.activity_fourth_exercise);
         recyclerView = findViewById(R.id.recycler_view);
         wordImage = findViewById(R.id.WordImage);
+        firstoption = findViewById(R.id.Option1);
+        secondoption = findViewById(R.id.Option2);
+        thirdoption = findViewById(R.id.Option3);
         final Integer[] WordsImages = {R.drawable.dates, R.drawable.horse, R.drawable.television, R.drawable.frog, R.drawable.lemon,
                 R.drawable.book, R.drawable.stars, R.drawable.crescent, R.drawable.turtle, R.drawable.bulb, R.drawable.box,
                 R.drawable.snake, R.drawable.strawberry};
+        final String [] FirstOption = {"نقود","حصان","تلفاز","","","","نجوم","","","","","","فراولة"};
+        final String [] SecondOption = {"تمر","","","","ليمون","","","","","مصباح","صندوق","",""};
+        final String [] ThirdOption = {"دجاج","","","ضفدع","","كتاب","","هلال","سلحفاة","","","ثعبان",""};
+
+        final List<String> newFirstOption = new ArrayList<>();
+        for (String i : FirstOption) {
+            newFirstOption.add(i);
+        }
+        final List<String> newSecondOption = new ArrayList<>();
+        for (String i : SecondOption){
+            newSecondOption.add(i);
+        }
+        final List<String> newThirdOption = new ArrayList<>();
+        for (String i : ThirdOption){
+            newThirdOption.add(i);
+        }
 
         modelFourthExercise = new ArrayList<>();
         for (int i = 0; i < WordsImages.length; i++) {
@@ -44,6 +68,9 @@ public class ActivityFourthExercise extends AppCompatActivity implements Adapter
             @Override
             public void onItemClick(int position) {
                 wordImage.setImageResource(WordsImages[position]);
+                firstoption.setText(newFirstOption.get(position));
+                secondoption.setText(newSecondOption.get(position));
+                thirdoption.setText(newThirdOption.get(position));
             }
         });
         recyclerView.setAdapter(adapterFourthExercise);
